42ba4488c312d5c95135a0aba8aee74b2b6673bf
Muhammad-Usman36 <146926435+Muhammad-Usman36@users.noreply.github.com>
2023-11-15 16:13:21 +0500

    j

--- App.tsx
+++ App.tsx
@@ -1,23 +1,20 @@
-// App.js
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import Page1 from './Page1';
-import Page2 from './Page2';
-import Page3 from './Page3';
-
-const Stack = createStackNavigator();
+import { View, Text, StyleSheet } from 'react-native';
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Page1">
-        <Stack.Screen name="Page1" component={Page1} />
-        <Stack.Screen name="Page2" component={Page2} />
-        <Stack.Screen name="Page3" component={Page3} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <View style={styles.container}>
+      <Text>Hello, World!</Text>
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 
 export default App;
